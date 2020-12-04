0fd15cf9eb31889b5945aa7ee7da122de1032e5a
Ayoub LAAROBI <ayoub.laarobi@gmail.com>
2020-12-04 12:05:36 +0100

    re-organize packages

--- src/main/java/src/Money.java
+++ src/main/java/src/Money.java
@@ -1,3 +1,4 @@
+package src;
 
 public class Money {
 	private int fAmount;
--- src/main/java/test/MoneyTest.java
+++ src/main/java/test/MoneyTest.java
@@ -1,8 +1,11 @@
+package test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import src.Money;
 
 
 public class MoneyTest {
